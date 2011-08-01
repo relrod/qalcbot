442a7d40e009f179fec9e00f6c52cec31c16422f
Nick Markwell <nick@duckinator.net>
2011-08-01 00:08:01 -0400

    Fixed up so it has an actual time limit now...

--- qalc.rb
+++ qalc.rb
@@ -1,11 +1,29 @@
 require 'open3'
 
+TIMELIMIT=2
+
 def qalc(equation)
-  Open3.popen3("qalc") do |stdin, stdout, stderr|
-    stdin.puts equation
+  ret = nil
+  qalc_pid = nil
+  t = Thread.new do
+    Open3.popen2e("qalc") do |stdin, stdout, wait_thr|
+      qalc_pid = wait_thr.pid
+      stdin.puts equation
 
-    2.times { stdout.gets }
+      2.times { stdout.gets }
 
-    stdout.gets.strip
+      ret = stdout.gets.strip
+    end
   end
+
+  (0..TIMELIMIT).each do |i|
+    break unless t.alive?
+    if i == TIMELIMIT || !ret.nil?
+      t.kill
+      `kill -9 #{qalc_pid}`
+      ret = "Timelimit exceeded" if ret.nil?
+    end
+    sleep 1
+  end
+  ret
 end
